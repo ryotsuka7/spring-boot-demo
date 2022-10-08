805e01aae22ae040667bf21a16634422c36ae795
ryotsuka <shinwa.otsuka@gmail.com>
2022-10-08 12:06:15 +0900

    add hello Web API.

---
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.springbootdemo.controller;
+
+import com.example.springbootdemo.dto.Sample;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public Sample hello(){
+        Sample sample = new Sample();
+        sample.setId(100);
+        sample.setName("taro");
+
+        return  sample;
+    }
+}
---
+++ src/main/java/com/example/springbootdemo/dto/Sample.java
@@ -0,0 +1,9 @@
+package com.example.springbootdemo.dto;
+
+import lombok.Data;
+
+@Data
+public class Sample {
+    private int id;
+    private String name;
+}
