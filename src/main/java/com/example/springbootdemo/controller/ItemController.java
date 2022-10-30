35276e8e0570ff17d0910838818d829a97ccfcfd
ryotsuka <shinwa.otsuka@gmail.com>
2022-10-30 21:39:38 +0900

    DBからPK値を指定してデータを取得するAPIを修正。

--- src/main/java/com/example/springbootdemo/controller/ItemController.java
+++ src/main/java/com/example/springbootdemo/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.example.springbootdemo.dto.ItemResponse;
 import com.example.springbootdemo.dto.Sample;
 import com.example.springbootdemo.entity.Item;
 import com.example.springbootdemo.mapper.ItemMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,13 +20,12 @@ public class ItemController {
 
     @GetMapping("/{id}")
     public ItemResponse findById(@PathVariable int id){
+        // DBからidをキーにデータを取得
         Item item = itemMapper.findById(id);
-        System.out.println(item);
 
+        // Responseにデータをコピーしてreturn
         ItemResponse itemResponse = new ItemResponse();
-        // TODO: Beanのコピーをすること
-        itemResponse.setId(item.getId());
-        itemResponse.setItemName(item.getItemName());
+        BeanUtils.copyProperties(item, itemResponse);
         return  itemResponse;
     }
 }
--- src/main/java/com/example/springbootdemo/mapper/ItemMapper.java
+++ src/main/java/com/example/springbootdemo/mapper/ItemMapper.java
@@ -6,6 +6,5 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ItemMapper {
-    //@Select("select i.id , i.item_name from item i where i.id  = #{id}")
     Item findById(int id);
 }
